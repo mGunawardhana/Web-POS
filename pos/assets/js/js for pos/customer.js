44884164b86911b9a749de2e67b86552f893b481
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-10-05 22:09:05 +0530

    initial commit

---
+++ pos/assets/js/js for pos/customer.js
@@ -0,0 +1,159 @@
+let customerDetails = [];
+
+/** SWEAT ALERTS */
+/* sweat alert for saved successfully */
+function savedSuccessfully() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-end',
+        background: '#0B9F43FF',
+        color: '#ffffff',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    Toast.fire({
+        icon: 'success',
+        title: 'saved in successfully'
+    })
+}
+
+/* sweat alerts for search result not found */
+function searchResultNotFound() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-end',
+        background: '#ff4757',
+        color: '#ffffff',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    Toast.fire({
+        icon: 'error',
+        title: "search result not found !"
+    })
+}
+
+/** CUSTOMER PAGE OPTIONS  */
+
+/* clear customer details */
+function clearCustomerTextFields() {
+    $('#cusID,#cusName,#cusAddress,#cusContact').val('');
+}
+
+/* load all Customer to table */
+function loadAllCustomer() {
+    /* removing repeating issue */
+    $("#customerTableBody").empty();
+
+    for (let i of customerDetails) {
+        let row = `<tr><td>${i.id}</td><td>${i.name}</td><td>${i.address}</td><td>${i.contact}</td></tr>`;
+        $('#customerTableBody').append(row);
+    }
+}
+
+/* disabling tab */
+$('#cusName,#cusAddress,#cusContact,#cusID').on('keydown', function (e) {
+    if (e.key === "Tab") {
+        e.preventDefault()
+    }
+})
+
+/* shift next text field */
+$('#cusID').on('keydown', function (e) {
+    if (e.key === "Enter") {
+        $('#cusName').focus()
+    }
+})
+$('#cusName').on('keydown', function (e) {
+    if (e.key === "Enter") {
+        $('#cusAddress').focus()
+    }
+})
+$('#cusAddress').on('keydown', function (e) {
+    if (e.key === "Enter") {
+        $('#cusContact').focus()
+    }
+})
+$('#cusContact').on('keydown', function (e) {
+    if (e.key === "Enter") {
+        saveCustomer()
+        $('#cusID').focus()
+    }
+
+})
+
+/* save customer option */
+function saveCustomer() {
+
+
+    /* packing customer details into customerObject */
+    let customerObject = {
+        id: $('#cusID').val(),
+        name: $('#cusName').val(),
+        address: $('#cusAddress').val(),
+        contact: $('#cusContact').val()
+    }
+
+    console.log(customerObject.toString())
+
+    clearCustomerTextFields();
+
+
+    /* saved notification for customer */
+    savedSuccessfully();
+
+    /* customerObject adding into customerDetails object holding Array */
+    customerDetails.push(customerObject);
+
+    loadAllCustomer()
+}
+
+/* saving customer object */
+$("#addCustomerBtn").on('click', function () {
+    saveCustomer()
+});
+
+/* search customer */
+function searchByCustomerID(id) {
+    for (let i = 0; i < customerDetails.length; i++) {
+        if (customerDetails[i].id === id) {
+            return customerDetails[i];
+        }
+    }
+}
+
+/* search by pressing enter option in customer form */
+$("#srcCustomerId").on('keydown', function (e) {
+
+    let response = searchByCustomerID($("#srcCustomerId").val());
+    let key = e.which;
+
+    if (key === 13) {
+        if (response) {
+            $("#cusID").val(response.id);
+            $("#cusName").val(response.name);
+            $("#cusAddress").val(response.address);
+            $('#cusContact').val(response.contact);
+        } else {
+            clearCustomerTextFields();
+            searchResultNotFound();
+        }
+    }
+});
+
+
+
+
+
+
+
---
+++ pos/assets/js/js for pos/item.js
@@ -0,0 +1,204 @@
+let itemDetails = [];
+
+/** SWEAT ALERTS */
+/* sweat alert for saved successfully */
+function savedSuccessfully() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-end',
+        background: '#0B9F43FF',
+        color: '#ffffff',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    Toast.fire({
+        icon: 'success',
+        title: 'saved in successfully'
+    })
+}
+
+/* sweat alerts for search result not found */
+function searchResultNotFound() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-end',
+        background: '#ff4757',
+        color: '#ffffff',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    Toast.fire({
+        icon: 'error',
+        title: "search result not found !"
+    })
+}
+
+function invalidDataInputFormat(){
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'bottom-end',
+        background: '#ff4757',
+        color: '#ffffff',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    Toast.fire({
+        icon: 'error',
+        title: " invalid data type please check and re enter !"
+    })
+}
+
+/** ITEM PAGE OPTIONS - */
+
+/* clear item details */
+function clearItemTextFields() {
+    $('#itemIdTxt,#ItemNameTxt,#itemQtyTxt,#unitPriceTxt').val('');
+}
+
+/* load all items to table */
+function loadAllItems() {
+    /* removing table row repeating issue */
+    $("#itemTblBody").empty();
+
+    for (let i of itemDetails) {
+        let row = `<tr>
+                        <td>${i.id}</td>
+                        <td>${i.name}</td>
+                        <td>${i.qty}</td>
+                        <td>${i.unitPrice}</td>
+                   </tr>`;
+        $('#itemTblBody').append(row);
+    }
+}
+
+/* disabling tab press */
+$('#ItemNameTxt,#itemIdTxt,#itemQtyTxt,#unitPriceTxt').on('keydown', function (e) {
+    if (e.key === "Tab") {
+        e.preventDefault()
+    }
+})
+
+
+let validatorPatternItemCode = /^C001$/;
+let validatorPatternItemName = /^Jack Daniel$/;
+let validatorPatternItemPrice = /^$/;
+let validatorPatternItemQty = /^$/;
+
+/* ----------- shift next text field ----------- */
+$('#itemIdTxt').on('keyup', function (e) {
+
+    if (validatorPatternItemCode.test($('#itemIdTxt').val())){
+        $('#itemIdTxt').css('border', '3px solid green')
+        $('#itemIdTxtLbl').text('')
+        if (e.key === "Enter") {
+            $('#ItemNameTxt').focus()
+        }
+    }else{
+        $('#itemIdTxt').css('border', '3px solid red');
+        $('#itemIdTxtLbl').text("Your input can't be validated, Ex - C001  ")
+    }
+})
+$('#ItemNameTxt').on('keyup', function (e) {
+
+    if (validatorPatternItemName.test($('#ItemNameTxt').val())){
+        $('#ItemNameTxt').css('border', '3px solid green')
+        $('#itemNameTxtLbl').text('')
+        if (e.key === "Enter") {
+            $('#itemQtyTxt').focus()
+        }
+    }
+    else {
+        $('#ItemNameTxt').css('border', '3px solid red');
+        $('#itemNameTxtLbl').text("Your input can't be validated, Ex - Jack Daniel  ")
+    }
+
+
+})
+$('#itemQtyTxt').on('keydown', function (e) {
+    if (e.key === "Enter") {
+        $('#unitPriceTxt').focus()
+    }
+})
+$('#unitPriceTxt').on('keydown', function (e) {
+    if (e.key === "Enter") {
+        saveItem()
+        $('#itemIdTxt').focus()
+    }
+})
+/* -------------------------------------------- */
+
+/* save item details */
+function saveItem() {
+
+    /* packing item details into itemObject */
+    let itemObject = {
+        id: $("#itemIdTxt").val(),
+        name: $("#ItemNameTxt").val(),
+        qty: $("#itemQtyTxt").val(),
+        unitPrice: $('#unitPriceTxt').val()
+    }
+
+    clearItemTextFields()
+
+    /* itemObjects adding into itemDetails main Array */
+    itemDetails.push(itemObject);
+
+    loadAllItems()
+
+    /* saved notification */
+    savedSuccessfully();
+}
+
+/* adding item details to array */
+$("#addItemBtn").on('click', function () {
+    saveItem()
+});
+
+/* search item */
+function searchByItemID(id) {
+    for (let i = 0; i < itemDetails.length; i++) {
+        if (itemDetails[i].id === id) {
+            return itemDetails[i];
+        }
+    }
+}
+
+/* search by pressing enter option in item form */
+$("#srcItemID").on('keyup', function (e) {
+    let response = searchByItemID($("#srcItemID").val());
+    let key = e.which;
+    if (key === 13) {
+        if (response) {
+            $("#itemIdTxt").val(response.id);
+            $("#ItemNameTxt").val(response.name);
+            $("#itemQtyTxt").val(response.qty);
+            $('#unitPriceTxt').val(response.unitPrice);
+        } else {
+            clearItemTextFields();
+            searchResultNotFound();
+        }
+    }
+});
+
+
+
+
+
+
+
+
